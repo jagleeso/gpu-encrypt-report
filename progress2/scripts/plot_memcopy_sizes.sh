070e002b5d232ca45371dc9d61763c909254c907
James <jagleeso@gmail.com>
2014-01-12 18:54:32 -0500

    combine cpu-gpu communication stuff into a single graph

--- progress2/scripts/plot_memcopy_sizes.sh
+++ progress2/scripts/plot_memcopy_sizes.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-file="$1"
+graph="$1"
 subtitle="$2"
+file1="$3"
+label_suffix1="$4"
+file2="$5"
+label_suffix2="$6"
+
 set -e
-shift 2 || 
-    (echo 2>&1 "ERROR: file subtitle" && exit 1)
+shift 6 || 
+    (echo 2>&1 "ERROR: graph subtitle file1 label_suffix1 file2 label_suffix2" && exit 1)
 
 # set -x
 
@@ -33,13 +38,34 @@ if (/^average GPU-to-CPU copy time: ([^ ]+) ms/) {
 
 # set -x
 for extractor in "$extract_memcopy_cpu_to_gpu_sizes_points" "$extract_memcopy_gpu_to_cpu_sizes_points"; do
-    cat "$file" | perl -lne "$extractor"
-    spacer
+    for file in "$file1" "$file2"; do
+        cat "$file" | perl -lne "$extractor"
+        spacer
+    done
 done
 # exit
 
+declare -a cpu_gpu_args_return=()
+cpu_gpu_args() {
+    local file="$1"
+    local label_suffix="$2"
+    shift 2
+    cpu_gpu_args_return=( \
+        "$extract_memcopy_gpu_to_cpu_sizes_points" "GPU-to-CPU${label_suffix}" "$file"
+        "$extract_memcopy_cpu_to_gpu_sizes_points" "CPU-to-GPU${label_suffix}" "$file"
+    )
+}
+
 title="CPU-GPU Communication - $subtitle" 
-graph="${file%.*}.eps"
+# graph="${file%.*}.eps"
+
+declare -a args=()
+cpu_gpu_args "$file1" "$label_suffix1"
+args=("${args[@]}" "${cpu_gpu_args_return[@]}")
+cpu_gpu_args "$file2" "$label_suffix2"
+args=("${args[@]}" "${cpu_gpu_args_return[@]}")
+
 plot_throughput_vs_bytes_multiple "$@" "$graph" "$title" \
-    "$extract_memcopy_cpu_to_gpu_sizes_points" "CPU-to-GPU" "$file" \
-    "$extract_memcopy_gpu_to_cpu_sizes_points" "GPU-to-CPU" "$file" \
+    "${args[@]}"
+    # "$extract_memcopy_cpu_to_gpu_sizes_points" "CPU-to-GPU" "$file" \
+    # "$extract_memcopy_gpu_to_cpu_sizes_points" "GPU-to-CPU" "$file" \
